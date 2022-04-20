8277fb702d88539de573e0aa0e553dc76876701e
personcube <matthew.perrault@gmail.com>
2022-04-20 15:33:52 +0000

    remove bucket arg

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   version = "2.8.0"
   source  = "app.terraform.io/ampf-mperra1/s3-bucket/aws"
   bucket_prefix = "matthewperrault"
-  bucket = "mperra1-s3-bucket"
+  #bucket = "mperra1-s3-bucket"
   acl    = "private"
   versioning = {
     enabled = true
